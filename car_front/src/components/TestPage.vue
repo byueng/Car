94901ee1871598f43c8c226e0fb1c010ac98b4ec
jwm <wzskqjh@gmail.com>
2025-02-17 17:27:45 +0800

    add test page

---
+++ car_front/src/components/TestPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="test-page">
+      <h2>Test Page</h2>
+      <p>Congratulations! You have successfully navigated to the Test page.</p>
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: 'TestPage',
+    };
+</script>
+
+<style scoped>
+    .test-page {
+        padding: 20px;
+        text-align: center;
+    }
+</style>
+  
