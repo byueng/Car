f04df1a1001288ce30bd7eed775a989770c35b44
jwm <wzskqjh@gmail.com>
2025-02-20 17:37:36 +0800

    complete get profile

--- car_backend/user_info/views.py
+++ car_backend/user_info/views.py
@@ -24,14 +24,50 @@ def test(request):
 
 
 class ProfileView(APIView):
-    permission_classes = [IsAuthenticated]  
+    # permission_classes = [IsAuthenticated]  
 
+    @csrf_exempt
     def get(self, request):
-        profile = Profile.objects.get(user=request.user) 
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
+        '''
+        Get Http request, return the user information to front
 
+        ProfileSerializer format:
+        {
+            "phone": "user_phone"
+            "email": "user_email"
+            "birthdate": "user_birthdate"
+        }
+
+        :param request: Http request object
+        :return Respone: Json type respone and the format is same as ProfileSerializer's with status 200 if successfully
+        '''
+        account = request.GET.get('account')
+        try:
+            if not account:
+                return JsonResponse({"message": "用户不存在"}, status=404)
+            else:
+                user = User.objects.get(account=account)
+                profile = Profile.objects.get(user=user)
+                serializers = ProfileSerializer(profile)
+                return Response(serializers.data, status=200)
+        except: 
+            return Response({'message': '出错了，请重试'}, status=404)     
+                       
+    @csrf_exempt
     def put(self, request):
+        '''
+        Put Http request, update the user information from front
+
+        ProfileSerializer format:
+        {
+            "phone": "user_phone"
+            "email": "user_email"
+            "birthdate": "user_birthdate"
+        }
+
+        :param request: Http request object
+        :return Respone: Json type respone with status 200 if successfully
+        '''
         profile = Profile.objects.get(user=request.user)
         serializer = ProfileSerializer(profile, data=request.data)
         if serializer.is_valid():
@@ -95,7 +131,9 @@ def register(request):
             return JsonResponse({'message': '创建失败，新建用户名已存在，请重试'}, status=404)
         else:
             user = User(account=account, password=password)
+            profile = Profile(user=user)
             user.save()
+            profile.save()
             return JsonResponse({'message': '用户创建成功，2秒后自动跳转到登录界面'}, status=201)
     else:
         return JsonResponse({'message': '错误的请求类型'}, status=405)
